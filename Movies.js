b0db657e3455b512848e1d99c25aa84b981778dc
mogadalas <simranchoudhary2021@gmail.com>
2025-05-02 08:27:07 -0600

    updated Movies.js

--- Movies.js
+++ Movies.js
@@ -33,16 +33,17 @@ const MovieSchema = new mongoose.Schema({
 
 const MovieSchema = new mongoose.Schema({
   title: { type: String, required: true, index: true },
-  releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100']},
-  genre: { type: String, enum: [
-    'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
-  ], 
-},
+  releaseDate: { type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, 'Must be less than 2100'] },
+  genre: {
+    type: String, enum: [
+      'Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'
+    ],
+  },
   actors: [{
     actorName: String,
     characterName: String,
-    imageUrl: String,
   }],
-  })
+  imageUrl: String
+})
 
 module.exports = mongoose.model('Movie', MovieSchema);
